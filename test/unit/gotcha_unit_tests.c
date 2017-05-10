b4dc2a50ab3f0938e3d642409c15fadd10bfbf72
Matthew LeGendre <legendre1@llnl.gov>
2017-05-10 11:32:18 -0700

    Add unit test for gotcha internal heap functions

--- test/unit/gotcha_unit_tests.c
+++ test/unit/gotcha_unit_tests.c
@@ -117,7 +117,61 @@ END_TEST
 START_TEST(gotcha_free_test){
   int* x = (int*)gotcha_malloc(sizeof(int)*10);
   gotcha_free(x);
+}
+END_TEST
 
+#define HEAP_TEST_SIZE 4096
+static int check_heap_test(int **allocations)
+{
+   int i, j;
+   for (i = 0; i < HEAP_TEST_SIZE; i++)
+   {
+      if (!allocations[i])
+         continue;
+      for (j = 0; j < i; j++) {
+         if (allocations[i][j] != i+j)
+            return -1;
+      }
+   }
+   return 0;
+}
+
+START_TEST(gotcha_heap_test)
+{
+   int i, j, k, l;
+   int **allocations;
+
+   //Do this test 10 times
+   for (l = 0; l < 10; l++) {
+      //Initialize allocations array to empty
+      allocations = (int **) gotcha_malloc(sizeof(int*) * HEAP_TEST_SIZE);
+      for (i = 0; i < HEAP_TEST_SIZE; i++) {
+         allocations[i] = NULL;
+      }
+      ck_assert(check_heap_test(allocations) == 0);
+
+      //Allocate each allocations[x] to be size x, and
+      //allocations[x][y] to be x+y.  Do in sets of x%3.
+      for (i = 0; i < 3; i++) {
+         for (j = i; j < HEAP_TEST_SIZE; j += 3) {
+            allocations[j] = (int *) gotcha_malloc(sizeof(int) * j);
+            for (k = 0; k < j; k++) {
+               allocations[j][k] = j+k;
+            }
+         }
+         ck_assert(check_heap_test(allocations) == 0);
+      }
+
+      //Free allocations in sets of x%5
+      for (i = 0; i < 5; i++) {
+         for (j = i; j < HEAP_TEST_SIZE; j += 5) {
+            gotcha_free(allocations[j]);
+            allocations[j] = NULL;
+         }
+         ck_assert(check_heap_test(allocations) == 0);
+      }
+      gotcha_free(allocations);
+   }
 }
 END_TEST
 
@@ -163,6 +217,7 @@ Suite* gotcha_libc_suite(){
   tcase_add_test(libc_case, gotcha_malloc_test);
   tcase_add_test(libc_case, gotcha_free_test);
   tcase_add_test(libc_case, gotcha_realloc_test);
+  tcase_add_test(libc_case, gotcha_heap_test);
   tcase_add_test(libc_case, gotcha_memcpy_test);
   tcase_add_test(libc_case, gotcha_strcmp_test);
   suite_add_tcase(s, libc_case);
