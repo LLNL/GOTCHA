ddec537d4acecd670fb87d2bfb8bcbf3505feeac
Matthew LeGendre <legendre1@llnl.gov>
2017-05-17 13:16:20 -0700

    Create hammer test, which hits gotcha with thousands of libraries and symbols

---
+++ test/hammer/gen.cc
@@ -0,0 +1,72 @@
+/*
+This file is part of GOTCHA.  For copyright information see the COPYRIGHT
+file in the top level directory, or at
+https://github.com/LLNL/gotcha/blob/master/COPYRIGHT
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License (as published by the Free
+Software Foundation) version 2.1 dated February 1999.  This program is
+distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the IMPLIED WARRANTY OF MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE. See the terms and conditions of the GNU Lesser General Public License
+for more details.  You should have received a copy of the GNU Lesser General
+Public License along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+*/
+
+#if !defined(_GNU_SOURCE)
+#define _GNU_SOURCE
+#endif
+
+#include "math.hpp"
+
+using namespace std;
+
+template <int A, int B>
+class Generate {
+public:
+   static void init() {
+      Mult<A, B>::init();
+      Add<A, B>::init();
+      Neg<A, B>::init();
+
+      //Extra gens here prevents recursive template overflow
+      (void) Generate<A, B/2>::init;  
+      (void) Generate<A/2, B>::init;  
+
+      Generate<A, B-1>::init();
+   }
+};
+
+template <int A>
+class Generate<A, 0> {
+public:
+   static void init() {
+      Mult<A, 0>::init();
+      Add<A, 0>::init();
+      Neg<A, 0>::init();
+
+      Generate<A-1, X>::init();
+   }
+};
+
+template <>
+class Generate<0, 0> {
+public:
+   static void init() {
+      Mult<0, 0>::init();
+      Add<0, 0>::init();
+      Neg<0, 0>::init();
+   }
+};
+
+template<int A, int B>
+result_t (*Neg<A, B>::mathfn_mult)(void) = NULL;
+
+template<int A, int B>
+result_t (*Neg<A, B>::mathfn_add)(void) = NULL;
+
+void initMath()
+{
+   Generate<Y, X>::init();
+}
+
