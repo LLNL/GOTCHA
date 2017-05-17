febcacd3b709f351fec0bb80efa22cfc14180267
Matthew LeGendre <legendre1@llnl.gov>
2017-05-17 13:15:08 -0700

    Make toolname string const

--- include/gotcha/gotcha.h
+++ include/gotcha/gotcha.h
@@ -32,6 +32,9 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
 #include "gotcha/gotcha_types.h"
 
+#if defined(__cplusplus) 
+extern "C" {
+#endif
 
 /*!
  ******************************************************************************
@@ -63,6 +66,10 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
  ******************************************************************************
  */
-enum gotcha_error_t gotcha_wrap(struct gotcha_binding_t* bindings, int num_actions, char* tool_name);
+enum gotcha_error_t gotcha_wrap(struct gotcha_binding_t* bindings, int num_actions, const char* tool_name);
+
+#if defined(__cplusplus) 
+}
+#endif
 
 #endif
