15b86f41de496ed9bc886cf94aeb81f917fcf7fb
David Poliakoff <david.poliakoff@gmail.com>
2018-03-22 06:33:37 -0700

    Unused parameter fix

--- src/translations.c
+++ src/translations.c
@@ -14,6 +14,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 #include <gotcha/gotcha.h>
 #include "translations.h"
+#include "gotcha_utils.h"
 /** "int main" wrapping handling */
 int gotcha_main_wrapper(int* argc, char** argv);
 //gotcha_wrappee_handle_t gotcha_internal_libc_main_wrappee_handle;
@@ -22,7 +23,7 @@ int gotcha_internal_main(int argc, char** argv, char** envp){
   main_t underlying_main = gotcha_get_wrappee(gotcha_internal_main_wrappee_handle); 
   return underlying_main(argc, argv, envp);
 }
-int gotcha_internal_libc_start_main(int (*main_arg)(int, char**, char**), int argc, char** argv, void (*init)(), void (*fini)(), void (*rtld_fini)(), void* stack_end){
+int gotcha_internal_libc_start_main(int (*main_arg)(int, char**, char**) KNOWN_UNUSED, int argc, char** argv, void (*init)(), void (*fini)(), void (*rtld_fini)(), void* stack_end){
    libc_start_main_t underlying_libc_main = gotcha_get_wrappee(gotcha_internal_libc_main_wrappee_handle);
    main_t underlying_main = gotcha_get_wrappee(gotcha_internal_main_wrappee_handle);
    return underlying_libc_main(underlying_main, argc, argv, init, fini, rtld_fini, stack_end);
