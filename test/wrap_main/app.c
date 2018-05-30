a5a312df909e29e1d0ad34a01abe91333fb1efa1
David Poliakoff <david.poliakoff@gmail.com>
2018-05-30 13:40:22 -0700

    Cleaned up the debug work

--- test/wrap_main/app.c
+++ test/wrap_main/app.c
@@ -1,12 +1,6 @@
 #include <stdio.h>
 #include "lib.h"
-#include <link.h>
 int main(){
-  struct link_map* my_iter= _r_debug.r_map;
-  for(;my_iter!=NULL;my_iter=my_iter->l_next){
-    printf("Have library %s at %lx\n",my_iter->l_name?:"NULL",my_iter->l_addr);
-
-  }
   printf("In main main\n");
-  return 1; // Should return 0
+  return getX();
 }
