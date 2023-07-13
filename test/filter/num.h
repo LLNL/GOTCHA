e779e0fd9e73ae827b9f52deae0db229686eab3a
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-07-13 00:13:09 -0700

    Added coverage for filter functions.

---
+++ test/filter/num.h
@@ -0,0 +1,10 @@
+//
+// Created by haridev on 7/12/23.
+//
+
+#ifndef GOTCHA_NUM_H
+#define GOTCHA_NUM_H
+extern int return_four();
+extern int return_five();
+extern int return_six();
+#endif //GOTCHA_NUM_H
