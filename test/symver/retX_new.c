dc3675de6acaaf0bde4b59c1edb2e4a21ebbab14
Hariharan Devarajan <mani.hariharan@gmail.com>
2023-12-16 16:37:15 +0000

    fixed spacing in code.

--- test/symver/retX_new.c
+++ test/symver/retX_new.c
@@ -13,5 +13,5 @@ Public License along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 */
 #include "sym_macro.h"
-SYMVER_ATTRIBUTE(retX_new, retX @ @GOTCHA_2)
+SYMVER_ATTRIBUTE(retX_new, retX@@GOTCHA_2)
 int retX_new(int x) { return x; }
